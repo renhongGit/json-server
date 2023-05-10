344b34342260b84f6c0d9e923e4e5ae3d78f3fdc
renhongGit <128814778+renhongGit@users.noreply.github.com>
2023-05-10 18:05:31 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -32,23 +32,24 @@ server.use((req, res, next) => {
   next();
 });
 
-// 更改路由的基础路径为 /api
-const apiRouter = jsonServer.router('db.json', {
-  foreignKeySuffix: '_id',
-  "/shopping": "shopping",
-  "/commodity": "Commodity",
-  "/userShopping": "userShopping",
-  "/laundry": "laundry"
-})
-server.use('/api', apiRouter)
+// 设置允许跨域请求的地址和方法
+const corsOptions = {
+  origin: 'http://localhost:8080',
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Headers'],
+  credentials: true
+}
 
-// 設置withCredentials: true
+// 使用中间件设置 CORS 头
+server.use(cors(corsOptions));
+
+// 允许预检请求的方法和头
+server.options('*', cors(corsOptions));
+
+// 设置请求的 CORS 头
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   res.header('Access-Control-Allow-Credentials', true);
-  req.headers.withCredentials = true;
   next();
 });
 
