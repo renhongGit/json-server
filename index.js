34c02a126a047f329d44456fb43544ea1adb69d8
renhongGit <128814778+renhongGit@users.noreply.github.com>
2023-05-10 21:39:48 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router('db.json', {
+  foreignKeySuffix: '_id',
+  "/shopping": "shopping",
+  "/commodity": "Commodity",
+  "/userShopping": "userShopping",
+  "/laundry": "laundry"
+})
 const middlewares = jsonServer.defaults()
-const cors = require('cors')
+
+const corsOptions = {
+  origin: "http://localhost:8080",
+  methods: ['GET', 'PUT', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+};
 
 server.use(middlewares)
-server.use(cors())
 
-server.use(router)
+server.use(cors(corsOptions));
+
+server.options('*', cors(corsOptions));
+
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+server.use(router);
 
 server.listen(3000, () => {
   console.log('JSON Server is running')
