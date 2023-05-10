cc3b766b53e01d3f0fa60e2f341bb1e611df63f5
renhongGit <128814778+renhongGit@users.noreply.github.com>
2023-05-10 14:29:18 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  
-    // intercepts OPTIONS method
-    if ('OPTIONS' === req.method) {
-      // respond with 200
-      res.sendStatus(200);
-    } else {
-      // move on
-      next();
-    }
-  });
+const cors = require('cors');
+const express = require('express');
+const jsonServer = require('json-server');
+
+const server = jsonServer.create();
+const router = jsonServer.router('commodity.json');
+const middlewares = jsonServer.defaults();
+
+// 设置 CORS 头，允许所有来源访问
+server.use(cors({
+  origin: '*',
+}));
+
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
