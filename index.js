ef8e4d89b9d959c27b6e124fb5b9966b275b846b
renhongGit <128814778+renhongGit@users.noreply.github.com>
2023-05-10 16:47:47 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('commodity.json', {
-    foreignKeySuffix: '_id',
-    "/shopping": "shopping",
-    "/commodity": "Commodity",
-    "/userShopping": "userShopping",
-    "/laundry": "laundry"
+  foreignKeySuffix: '_id',
+  "/shopping": "shopping",
+  "/commodity": "Commodity",
+  "/userShopping": "userShopping",
+  "/laundry": "laundry"
 })
 const middlewares = jsonServer.defaults()
 
@@ -14,22 +14,22 @@ const cors = require('cors');
 server.use(middlewares)
 
 server.use(cors({
-    origin: '*',
-    methods: ['GET', 'PUT', 'POST', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Headers']
+  origin: '*',
+  methods: ['GET', 'PUT', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Headers']
 }));
-
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
 
 server.options('*', cors());
 
-server.use('/api', router)
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  next();
+});
 
+server.use('/api', router)
 
 server.listen(3000, () => {
   console.log('JSON Server is running')
