64dc52ca74d06aa364a0449026569347a482538f
renhongGit <128814778+renhongGit@users.noreply.github.com>
2023-05-10 19:34:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   res.header('Access-Control-Allow-Credentials', true);
-  req.headers.withCredentials = true;  // 添加这行代码
+  req.headers.withCredentials = true;  
   next();
 });
 
-// 设置允许跨域请求的地址和方法
+
 const corsOptions = {
   origin: 'http://localhost:8080',
   methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
@@ -41,13 +41,13 @@ const corsOptions = {
   credentials: true
 }
 
-// 使用中间件设置 CORS 头
+
 server.use(cors(corsOptions));
 
-// 允许预检请求的方法和头
+
 server.options('*', cors(corsOptions));
 
-// 设置请求的 CORS 头
+
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
   next();
