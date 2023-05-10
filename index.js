8d29acd76cbbcf3822a7d380fcf8bd0fc14dce85
renhongGit <128814778+renhongGit@users.noreply.github.com>
2023-05-10 14:25:08 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
-const cors = require('cors');
-const express = require('express');
-const jsonServer = require('json-server');
-
-const server = jsonServer.create();
-const router = jsonServer.router('commodity.json');
-const middlewares = jsonServer.defaults();
-
-server.use(cors()); // 设置 CORS 头
-server.use(middlewares);
-
-// 在 JSON Server 路由之前添加自定义的中间件
 server.use((req, res, next) => {
-  // 允许所有来源的访问
-  res.header('Access-Control-Allow-Origin', '*');
-  // 允许的 HTTP 请求方法
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-  // 允许的 HTTP 请求头部
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  // 跨域请求会首先发送一个 OPTIONS 请求，这里给OPTIONS请求返回200状态码，以便浏览器继续进行
-  if (req.method === 'OPTIONS') {
-    res.status(200).send();
-  } else {
-    next();
-  }
-});
-
-server.use(router);
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  
+    // intercepts OPTIONS method
+    if ('OPTIONS' === req.method) {
+      // respond with 200
+      res.sendStatus(200);
+    } else {
+      // move on
+      next();
+    }
+  });
