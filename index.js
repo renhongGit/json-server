5fc1f25b254603faddc85c6d9c0483feeb902a53
renhongGit <128814778+renhongGit@users.noreply.github.com>
2023-05-10 21:26:10 +0800

    Add files via upload
    server.use((req, res, next) => {
      res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
      res.header('Access-Control-Allow-Credentials', true);
      next();
    });

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ server.use(cors(corsOptions));
 server.options('*', cors(corsOptions));
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', true);
-  req.headers.withCredentials = true;  
   next();
 });
 
