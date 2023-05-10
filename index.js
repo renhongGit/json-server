fc89d6104df095e9b20974bad1d681785bec8242
renhongGit <128814778+renhongGit@users.noreply.github.com>
2023-05-10 17:41:12 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -14,18 +14,20 @@ const cors = require('cors');
 server.use(middlewares)
 
 server.use(cors({
-  origin: '*',
+  origin: 'http://localhost:8080',
   methods: ['GET', 'PUT', 'POST', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  exposedHeaders: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Headers']
+  exposedHeaders: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Headers'],
+  credentials: true
 }));
 
 server.options('*', cors());
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 
