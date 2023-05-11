c42119c16528c7649e2e6edcd2bc80dbfb027a6a
renhongGit <128814778+renhongGit@users.noreply.github.com>
2023-05-11 19:29:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
 const router = jsonServer.router('db.json', {
   foreignKeySuffix: '_id',
   "/shopping": "shopping",
   "/commodity": "Commodity",
   "/userShopping": "userShopping",
   "/laundry": "laundry"
-})
-const middlewares = jsonServer.defaults()
-
+});
+const middlewares = jsonServer.defaults();
 const cors = require('cors');
 
-server.use(middlewares)
+server.use(middlewares);
 
 server.use(cors({
-  origin: "http://localhost:8080",
+  origin: true,
   methods: ['GET', 'PUT', 'POST', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
@@ -23,7 +22,7 @@ server.use(cors({
 server.options('*', cors());
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "http://localhost:8080");
+  res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   res.header('Access-Control-Allow-Credentials', true);
@@ -31,8 +30,17 @@ server.use((req, res, next) => {
   next();
 });
 
+// 添加以下程式碼段以將VerceI設置為代理服務器
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = createProxyMiddleware('/api', {
+  target: 'http://localhost:3000',
+  changeOrigin: true,
+});
+
+server.use('/api', proxy);
+
 server.use(router);
 
 server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+  console.log('JSON Server is running');
+});
